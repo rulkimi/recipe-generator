39a82f62c7129bcd9290244ca74f4d5fabf64274
rulkimi <azrulkimi00@gmail.com>
2024-07-28 01:36:52 +0800

    fix some colors and added headers

--- client/src/components/ui/dialog/DialogDescription.vue
+++ client/src/components/ui/dialog/DialogDescription.vue
@@ -17,7 +17,7 @@ const forwardedProps = useForwardProps(delegatedProps)
 <template>
   <DialogDescription
     v-bind="forwardedProps"
-    :class="cn('text-sm text-muted-foreground', props.class)"
+    :class="cn('text-sm', props.class)"
   >
     <slot />
   </DialogDescription>

85370fcf0ab3957fdcf164272dc95672f864f2af
rulkimi <azrulkimi00@gmail.com>
2024-12-21 11:14:51 +0800

    add nav menus

--- react-client/src/components/app-header.tsx
+++ react-client/src/components/app-header.tsx
@@ -6,27 +6,47 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+interface NavMenu {
+  label: string;
+  url: string;
+}
+
+const navMenus: NavMenu[] = [
+  { label: 'Generate', url: '' },
+  { label: 'Saved Recipes', url: '' },
+]
+
 const AppHeader = () => {
   return (
     <header className="border-b shadow-sm p-4">
-      <div className="container mx-auto flex justify-center items-center gap-3">
-        <span className="text-2xl font-bold">Recipe Generator</span>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <img
-                src={GithubMark}
-                className="cursor-pointer scale-transition"
-                alt="Github Mark"
-                width={25}
-                onClick={() => window.open('https://github.com/rulkimi/recipe-generator', '_blank')}
-              />
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>Github Repository</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+      <div className="container mx-auto flex items-center justify-between gap-4">
+        <div className="flex gap-2">
+          <img src="/logo.svg" alt="App Logo" width={25}/>
+          <span className="text-xl font-bold">Recipe Generator</span>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <nav className="flex items-center gap-4">
+            {navMenus.map(menu => (
+              <div className="hover:text-zinc-600 cursor-pointer">{menu.label}</div>
+            ))}
+          </nav>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <img
+                  src={GithubMark}
+                  className="cursor-pointer scale-transition aspect-square w-[25px] h-[25px]"
+                  alt="Github Mark"
+                  onClick={() => window.open('https://github.com/rulkimi/recipe-generator', '_blank')}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Github Repository</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
     </header>
   )

72acb08103011aa58c6778e92de39aef6ffae529
rulkimi <azrulkimi00@gmail.com>
2025-11-20 20:44:13 +0800

    change to 2.5 flash

--- server/core/model.py
+++ server/core/model.py
@@ -7,6 +7,6 @@ load_dotenv()
 def get_model():
     genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
     return genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-2.5-flash",
         generation_config={"response_mime_type": "application/json"},
     )
